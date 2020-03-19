4dc535bf2e8fcf458b8252bf4b2ec0294f613eb8
Sylvain Moindron <sylvain.moindron@sword-group.com>
2020-03-19 11:51:31 +0100

    :sparkles: ihm:login recherche maintenant l'utilisateur en bdd

--- frontend/ihm/src/main/kotlin/com/sword/signature/ihm/configuration/BeansInitialiser.kt
+++ frontend/ihm/src/main/kotlin/com/sword/signature/ihm/configuration/BeansInitialiser.kt
@@ -1,25 +1,23 @@
 package com.sword.signature.ihm.configuration
 
+import com.sword.signature.business.service.AccountService
 import com.sword.signature.ihm.controller.MainHandler
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.support.GenericApplicationContext
 import org.springframework.context.support.beans
 import org.springframework.core.io.ClassPathResource
 import org.springframework.http.MediaType.TEXT_HTML
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.servlet.function.router
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect
 
 
 fun bean() = beans {
     bean {
-        //manager des utilisateur en meoire et completement pouri
-        // TODO mettre une vrai gestion des user
-        fun user(user: String, pw: String, vararg roles: String) =
-            User.withDefaultPasswordEncoder().username(user).password(pw).roles(*roles).build()
-
-        InMemoryUserDetailsManager(user("user", "user", "USER"), user("admin", "admin", "USER", "ADMIN"))
+        BCryptPasswordEncoder()
+    }
+    bean {
+        DatabaseAuthenticationProvider(ref(),ref())
     }
     bean {
         MainHandler()
--- frontend/ihm/src/main/kotlin/com/sword/signature/ihm/configuration/SecurityConfig.kt
+++ frontend/ihm/src/main/kotlin/com/sword/signature/ihm/configuration/SecurityConfig.kt
@@ -1,7 +1,9 @@
 package com.sword.signature.ihm.configuration
 
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -9,7 +11,9 @@ import org.springframework.security.config.web.servlet.invoke
 
 @EnableWebSecurity
 @Configuration
-class KotlinSecurityConfiguration : WebSecurityConfigurerAdapter() {
+class KotlinSecurityConfiguration(
+    private val databaseAuthenticationProvider :DatabaseAuthenticationProvider
+) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         http {
@@ -33,6 +37,11 @@ class KotlinSecurityConfiguration : WebSecurityConfigurerAdapter() {
                 failureUrl = "/loginerror"
             }
         }
+    }
+
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        auth.authenticationProvider(databaseAuthenticationProvider)
     }
 }
 
