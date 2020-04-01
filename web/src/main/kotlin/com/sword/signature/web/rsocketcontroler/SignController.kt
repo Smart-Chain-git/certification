a5c75237b3130ce96856ed9279e73076ace0e143
Sylvain Moindron <sylvain.moindron@sword-group.com>
2020-04-01 10:46:23 +0200

    :sparkles: Verification de l'algorithme passe en parametre

--- web/src/main/kotlin/com/sword/signature/web/rsocketcontroler/SignController.kt
+++ web/src/main/kotlin/com/sword/signature/web/rsocketcontroler/SignController.kt
@@ -4,6 +4,7 @@ import com.sword.signature.api.sign.SignRequest
 import com.sword.signature.api.sign.SignResponse
 import com.sword.signature.business.exception.AccountNotFoundException
 import com.sword.signature.business.service.AccountService
+import com.sword.signature.business.service.AlgorithmService
 import com.sword.signature.business.service.SignService
 import com.sword.signature.web.mapper.toBusiness
 import com.sword.signature.web.mapper.toWeb
@@ -14,21 +15,30 @@ import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Controller
+import java.lang.IllegalArgumentException
 
 @Controller
 class SignController(
     val accountService: AccountService,
-    val signService: SignService
+    val signService: SignService,
+    val algorithmService: AlgorithmService
 ) {
 
     @MessageMapping("newJobs")
     suspend fun batchSign(
         @AuthenticationPrincipal user: UserDetails,
-        @Header(name = "algorithm") algorithm: String,
+        @Header(name = "algorithm") algorithm: String?,
       requests: Flow<SignRequest>
     ): Flow<SignResponse> {
         val account =
             accountService.getAccountByLoginOrEmail(user.username) ?: throw AccountNotFoundException(user.username)
+
+        if(algorithm == null) {
+            throw IllegalArgumentException("missing algorithm parameter")
+        }
+
+        algorithmService.getAlgorithmByName(algorithm)
+
         return signService.batchSign(account, algorithm, requests.map { it.toBusiness() }).map { it.toWeb() }
     }
 }
