8786088012cad93f6cfb8c5dd89d5f75a47b0ebc
Sylvain Moindron <sylvain.moindron@sword-group.com>
2020-04-09 11:30:20 +0200

    :sparkles: Liste des jobs sur la page des jobs

--- web/src/main/kotlin/com/sword/signature/web/webhandler/JobHandler.kt
+++ web/src/main/kotlin/com/sword/signature/web/webhandler/JobHandler.kt
@@ -1,5 +1,7 @@
 package com.sword.signature.web.webhandler
 
+import com.sword.signature.business.service.JobService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -7,14 +9,24 @@ import org.springframework.web.reactive.function.server.html
 import org.springframework.web.reactive.function.server.renderAndAwait
 
 @Component
-class JobHandler {
+class JobHandler(
+    private val jobService: JobService
+) {
 
 
     suspend fun jobs(request: ServerRequest): ServerResponse {
-        val model = mapOf<String, String>()
+        val account = request.getAccount()
+        LOGGER.info("{} demande ses jobs", account)
+        val jobs=jobService.findAllByUser(account)
+        val model = mapOf<String, Any>(
+            "jobs" to jobs
+        )
         return ServerResponse.ok().html().renderAndAwait("jobs/jobs", model)
     }
 
 
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(JobHandler::class.java)
+    }
 
 }
