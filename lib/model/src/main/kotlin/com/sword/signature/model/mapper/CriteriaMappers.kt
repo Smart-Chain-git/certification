fbbb766491726a7e0b5c5cda936660743aeaee44
Sylvain Moindron <sylvain.moindron@sword-group.com>
2020-04-16 10:57:16 +0200

    :recycle: lors des requete Mongo plus besoin d'ajouter une tautologie pour les requete vide

--- lib/model/src/main/kotlin/com/sword/signature/model/mapper/CriteriaMappers.kt
+++ lib/model/src/main/kotlin/com/sword/signature/model/mapper/CriteriaMappers.kt
@@ -1,10 +1,8 @@
 package com.sword.signature.model.mapper
 
 import com.querydsl.core.BooleanBuilder
-import com.querydsl.core.types.ExpressionUtils
-import com.sword.signature.common.criteria.TreeElementCriteria
 import com.querydsl.core.types.Predicate
-import com.querydsl.core.types.dsl.Expressions
+import com.sword.signature.common.criteria.TreeElementCriteria
 import com.sword.signature.model.entity.QTreeElementEntity
 
 fun TreeElementCriteria.toPredicate(): Predicate {
@@ -12,8 +10,6 @@ fun TreeElementCriteria.toPredicate(): Predicate {
     val predicates = ArrayList<Predicate>()
 
     val qTreeElementEntity = QTreeElementEntity("blop")
-    predicates.add(qTreeElementEntity.id.isNotNull) //dirty pour avoir au moin un predicat
-
 
     if (this.jobId?.isNotBlank() == true) {
         predicates.add(
@@ -29,10 +25,8 @@ fun TreeElementCriteria.toPredicate(): Predicate {
     return andTogetherPredicates(predicates)
 }
 
-private fun andTogetherPredicates(predicates: List<Predicate>): Predicate {
-        val bb = BooleanBuilder()
-        predicates.forEach {
-            bb.and(it)
-        }
-      return   bb
+private fun andTogetherPredicates(predicates: List<Predicate>) = BooleanBuilder().apply {
+    predicates.forEach {
+        and(it)
+    }
 }
