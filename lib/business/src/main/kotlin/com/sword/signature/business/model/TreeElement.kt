f555f402e73924f45542e0a2c7d56c8d1a13662a
Sylvain Moindron <sylvain.moindron@sword-group.com>
2020-04-20 16:52:27 +0200

    :construction: lib:business Ajout de hashCode et equals à TreeElement

--- lib/business/src/main/kotlin/com/sword/signature/business/model/TreeElement.kt
+++ lib/business/src/main/kotlin/com/sword/signature/business/model/TreeElement.kt
@@ -19,7 +19,23 @@ sealed class TreeElement(
         position: TreeElementPosition? = null,
         jobId: String,
         val fileName: String
-    ) : TreeElement(id = id, hash = hash, parentId = parentId, position = position, jobId = jobId)
+    ) : TreeElement(id = id, hash = hash, parentId = parentId, position = position, jobId = jobId) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other !is LeafTreeElement) return false
+            if (!super.equals(other)) return false
+
+            if (fileName != other.fileName) return false
+
+            return super.equals(other)
+        }
+
+        override fun hashCode(): Int {
+            var result = super.hashCode()
+            result = 31 * result + fileName.hashCode()
+            return result
+        }
+    }
 
     class NodeTreeElement(
         id: String,
@@ -28,4 +44,28 @@ sealed class TreeElement(
         position: TreeElementPosition? = null,
         jobId: String
     ) : TreeElement(id = id, hash = hash, parentId = parentId, position = position, jobId = jobId)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is TreeElement) return false
+
+        if (id != other.id) return false
+        if (hash != other.hash) return false
+        if (parentId != other.parentId) return false
+        if (position != other.position) return false
+        if (jobId != other.jobId) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + hash.hashCode()
+        result = 31 * result + (parentId?.hashCode() ?: 0)
+        result = 31 * result + (position?.hashCode() ?: 0)
+        result = 31 * result + jobId.hashCode()
+        return result
+    }
+
+
 }
