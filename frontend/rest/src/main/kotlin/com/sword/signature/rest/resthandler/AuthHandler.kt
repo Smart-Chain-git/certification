bbeb59b994b70b063d726f93dc0df1c08d4e21cf
Sylvain Moindron <sylvain.moindron@sword-group.com>
2020-05-15 09:21:59 +0200

    :construction_worker: :front:rest /auth vérification de l'identité

--- frontend/rest/src/main/kotlin/com/sword/signature/rest/resthandler/AuthHandler.kt
+++ frontend/rest/src/main/kotlin/com/sword/signature/rest/resthandler/AuthHandler.kt
@@ -2,12 +2,20 @@ package com.sword.signature.rest.resthandler
 
 import com.sword.signature.api.sign.AuthRequest
 import com.sword.signature.api.sign.AuthResponse
+import com.sword.signature.business.service.AccountService
+import com.sword.signature.webcore.authentication.UserDetailsService
 import io.swagger.v3.oas.annotations.Parameter
+import kotlinx.coroutines.reactive.awaitFirst
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("\${api.base-path:/api}")
-class AuthHandler {
+class AuthHandler (
+    private val accountService: AccountService,
+    private val bCryptPasswordEncoder: BCryptPasswordEncoder
+){
 
     @RequestMapping(
         value = ["/auth"],
@@ -21,6 +29,11 @@ class AuthHandler {
             required = true
         )@RequestBody request: AuthRequest
     ): AuthResponse {
+        val user = accountService.getAccountByLoginOrEmail(request.user)?:throw BadCredentialsException("Invalid credentials")
+
+        if (!bCryptPasswordEncoder.matches(request.password , user.password)) {
+            throw BadCredentialsException("Invalid credentials")
+        }
 
         return AuthResponse(token = "suptoken")
     }
