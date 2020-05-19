d67ad26eb36670f7f4eb2253290edb5b37907cbb
Sylvain Moindron <sylvain.moindron@sword-group.com>
2020-05-19 13:52:33 +0200

    :bug: #97 : :front:rsocket in spring fun ()-> Flow shouldn't be suspendable

--- frontend/rsocket/src/main/kotlin/com/sword/signature/rsocket/rsocketcontroler/SignController.kt
+++ frontend/rsocket/src/main/kotlin/com/sword/signature/rsocket/rsocketcontroler/SignController.kt
@@ -7,16 +7,16 @@ import com.sword.signature.business.service.AccountService
 import com.sword.signature.business.service.AlgorithmService
 import com.sword.signature.business.service.SignService
 import com.sword.signature.webcore.mapper.toBusiness
-import com.sword.signature.webcore.mapper.toWeb
 import com.sword.signature.webcore.mapper.toWebSignResponse
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Controller
-import java.lang.IllegalArgumentException
 
 @Controller
 class SignController(
@@ -26,25 +26,30 @@ class SignController(
 ) {
 
     @MessageMapping("newJobs")
-    suspend fun batchSign(
+    fun batchSign(
         @AuthenticationPrincipal user: UserDetails,
         @Header(name = "algorithm") algorithmParameter: String?,
         @Header(name = "flowName") flowName: String?,
         requests: Flow<SignRequest>
     ): Flow<SignResponse> {
-        val account =
-            accountService.getAccountByLoginOrEmail(user.username) ?: throw AccountNotFoundException(user.username)
 
         if (algorithmParameter == null) {
             throw IllegalArgumentException("missing algorithm parameter")
         }
+
         if (flowName == null) {
             throw IllegalArgumentException("missing flowName parameter")
         }
+        return flow {
+            val account =
+                accountService.getAccountByLoginOrEmail(user.username) ?: throw AccountNotFoundException(user.username)
 
-        val algorithm = algorithmService.getAlgorithmByName(algorithmParameter)
 
-        return signService.batchSign(account, algorithm, flowName, requests.map { it.toBusiness() }).map { it.toWebSignResponse() }
+            val algorithm = algorithmService.getAlgorithmByName(algorithmParameter)
+
+            emitAll(signService.batchSign(account, algorithm, flowName, requests.map { it.toBusiness() })
+                .map { it.toWebSignResponse() })
+        }
     }
 }
 
