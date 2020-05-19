fb16309bad7d5deb0b897f377b63d54ec37d852e
Sylvain Moindron <sylvain.moindron@sword-group.com>
2020-05-19 16:36:23 +0200

    :bug: #97 : :front:rsocket in netty spring, fun ()-> Flow shouldn't be suspendable

--- frontend/rsocket/src/main/kotlin/com/sword/signature/rsocket/rsocketcontroler/SignController.kt
+++ frontend/rsocket/src/main/kotlin/com/sword/signature/rsocket/rsocketcontroler/SignController.kt
@@ -9,9 +9,8 @@ import com.sword.signature.business.service.SignService
 import com.sword.signature.webcore.mapper.toBusiness
 import com.sword.signature.webcore.mapper.toWebSignResponse
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -40,16 +39,21 @@ class SignController(
         if (flowName == null) {
             throw IllegalArgumentException("missing flowName parameter")
         }
-        return flow {
-            val account =
-                accountService.getAccountByLoginOrEmail(user.username) ?: throw AccountNotFoundException(user.username)
 
-
-            val algorithm = algorithmService.getAlgorithmByName(algorithmParameter)
-
-            emitAll(signService.batchSign(account, algorithm, flowName, requests.map { it.toBusiness() })
-                .map { it.toWebSignResponse() })
+        // BOF BIDOUILLE
+        // je fait un runBlocking pour appeler un truc non bloquant
+        // car sinon  le return de mon flow me lance une erreur de netty
+        // c'ets tolerable car on bloque que sur un petit appel de la table des utilisateur et des algo
+        val account = runBlocking {
+            accountService.getAccountByLoginOrEmail(user.username) ?: throw AccountNotFoundException(user.username)
         }
+
+
+        val algorithm = runBlocking { algorithmService.getAlgorithmByName(algorithmParameter) }
+
+        return signService.batchSign(account, algorithm, flowName, requests.map { it.toBusiness() })
+            .map { it.toWebSignResponse() }
+
     }
 }
 
