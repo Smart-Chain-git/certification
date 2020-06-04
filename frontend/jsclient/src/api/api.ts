8a9a451ed8e556f8f707953b37d25976ba41ce40
Sylvain Moindron <sylvain.moindron@sword-group.com>
2020-06-04 11:54:49 +0200

    :lock: #109 :front:jsclient: add X-Requested-With to header

--- frontend/jsclient/src/api/api.ts
+++ frontend/jsclient/src/api/api.ts
@@ -1,42 +1,42 @@
-import globalAxios, {AxiosInstance, AxiosRequestConfig, AxiosResponse} from "axios"
-
+import globalAxios, { AxiosRequestConfig, AxiosResponse} from "axios"
 
 
 export class Api {
+    private config: AxiosRequestConfig
 
     public constructor(config: AxiosRequestConfig) {
-        //  nothing
+        this.config = config
     }
 
-    public getUri(config?: AxiosRequestConfig): string {
+    public getUri(config: AxiosRequestConfig = this.config): string {
         return globalAxios.getUri(config)
     }
 
-    public request<T, R = AxiosResponse<T>>(config: AxiosRequestConfig): Promise<R> {
+    public request<T, R = AxiosResponse<T>>(config: AxiosRequestConfig = this.config): Promise<R> {
         return globalAxios.request(config)
     }
 
-    public get<T, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
+    public get<T, R = AxiosResponse<T>>(url: string, config: AxiosRequestConfig = this.config): Promise<R> {
         return globalAxios.get(url, config)
     }
 
-    public delete<T, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
+    public delete<T, R = AxiosResponse<T>>(url: string, config: AxiosRequestConfig = this.config): Promise<R> {
         return globalAxios.delete(url, config)
     }
 
-    public head<T, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
+    public head<T, R = AxiosResponse<T>>(url: string, config: AxiosRequestConfig = this.config): Promise<R> {
         return globalAxios.head(url, config)
     }
 
-    public post<T, D, R = AxiosResponse<T>>(url: string, data: D, config?: AxiosRequestConfig): Promise<R> {
+    public post<T, D, R = AxiosResponse<T>>(url: string, data: D, config: AxiosRequestConfig = this.config): Promise<R> {
         return globalAxios.post(url, data, config)
     }
 
-    public put<T, D, R = AxiosResponse<T>>(url: string, data: D, config?: AxiosRequestConfig): Promise<R> {
+    public put<T, D, R = AxiosResponse<T>>(url: string, data: D, config: AxiosRequestConfig = this.config): Promise<R> {
         return globalAxios.put(url, data, config)
     }
 
-    public patch<T, R = AxiosResponse<T>>(url: string, data?: string, config?: AxiosRequestConfig): Promise<R> {
+    public patch<T, R = AxiosResponse<T>>(url: string, data?: string, config: AxiosRequestConfig = this.config): Promise<R> {
         return globalAxios.patch(url, data, config)
     }
 }
