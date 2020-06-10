4d143d8bfe962e8007b04b097577e4c38246b9ee
Yves Coriton <yves.coriton@sword-group.com>
2020-06-10 12:16:16 +0200

    :lock: Add encrypted password in configuration

--- backend/daemon/src/main/kotlin/com/sword/signature/daemon/configuration/DaemonConfiguration.kt
+++ backend/daemon/src/main/kotlin/com/sword/signature/daemon/configuration/DaemonConfiguration.kt
@@ -1,10 +1,10 @@
 package com.sword.signature.daemon.configuration
 
-
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
-
 @Configuration
+@EnableEncryptableProperties
 @ComponentScan(basePackages = ["com.sword.signature"])
 class DaemonConfiguration
