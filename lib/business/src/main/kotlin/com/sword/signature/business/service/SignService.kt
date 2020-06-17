a3b1fe4bfeef71f2d38436a53f5d14077b781551
Yves Coriton <yves.coriton@sword-group.com>
2020-06-17 18:44:47 +0200

    :sparkles: #113 Add fileHandler

--- lib/business/src/main/kotlin/com/sword/signature/business/service/SignService.kt
+++ lib/business/src/main/kotlin/com/sword/signature/business/service/SignService.kt
@@ -1,20 +1,29 @@
 package com.sword.signature.business.service
 
-import com.sword.signature.business.model.*
+import com.sword.signature.business.model.Account
+import com.sword.signature.business.model.Algorithm
+import com.sword.signature.business.model.FileMetadata
+import com.sword.signature.business.model.Job
 import kotlinx.coroutines.flow.Flow
 
 interface SignService {
 
-
+    /**
+     * Sign a batch of files.
+     * @param requester The requester of the sign operation.
+     * @param channelName The name of the channel used to sign.
+     * @param algorithm The algorithm used to hash the files and to use to build the merkle tree.
+     * @param flowName The name of the flow (only for identification).
+     * @param callBackUrl The URL to call once the merkle tree root hash is on the blockchain and validated.
+     * @param fileHashes The list of files to sign (first element: the hash, second element: the metadata).
+     * @return The list of jobs created during the sign operation.
+     */
     fun batchSign(
         requester: Account,
-        channelName : String?,
+        channelName: String?,
         algorithm: Algorithm,
         flowName: String,
         callBackUrl: String? = null,
-        fileHashs: Flow<Pair<String, FileMetadata>>
+        fileHashes: Flow<Pair<String, FileMetadata>>
     ): Flow<Job>
-
-    suspend fun getFileProof(requester: Account, fileId: String): Proof?
-
 }
--- lib/business/src/main/kotlin/com/sword/signature/business/service/impl/SignServiceImpl.kt
+++ lib/business/src/main/kotlin/com/sword/signature/business/service/impl/SignServiceImpl.kt
@@ -35,29 +35,8 @@ class SignServiceImpl(
     @Value("\${sign.tree.maximumLeaf}") val maximumLeaf: Int,
     private val jobRepository: JobRepository,
     private val treeElementRepository: TreeElementRepository,
-    private val anchoringMessageChannel: MessageChannel,
-    private val tezosReaderService: TezosReaderService
+    private val anchoringMessageChannel: MessageChannel
 ) : SignService {
-
-    // Local index of contract creators
-    private val contractCreators = mutableMapOf<String, String>()
-
-    private suspend fun getContractCreator(contractAddress: String): String? {
-        if (contractCreators.containsKey(contractAddress)) {
-            return contractCreators[contractAddress]
-        } else {
-            try {
-                val creator = tezosReaderService.getContract(contractAddress)
-                creator?.let {
-                    contractCreators[contractAddress] = it.manager
-                    return it.manager
-                }
-            } catch (e: Exception) {
-                LOGGER.error("Indexer can't be used to retrieve contract creator.")
-            }
-            return null
-        }
-    }
 
     @Transactional(rollbackFor = [Exception::class])
     override fun batchSign(
@@ -66,14 +45,14 @@ class SignServiceImpl(
         algorithm: Algorithm,
         flowName: String,
         callBackUrl: String?,
-        fileHashs: Flow<Pair<String, FileMetadata>>
+        fileHashes: Flow<Pair<String, FileMetadata>>
     ): Flow<Job> {
 
         return flow {
 
             // intermediary doit etre declaré dans le scope du flow!
             val intermediary = mutableListOf<Pair<String, FileMetadata>>()
-            fileHashs.collect { fileHash ->
+            fileHashes.collect { fileHash ->
                 if (!algorithm.checkHashDigest(fileHash.first)) {
                     throw UserServiceException("bad ${algorithm.name} hash for file ${fileHash.second}")
                 }
@@ -96,7 +75,7 @@ class SignServiceImpl(
         algorithm: Algorithm,
         flowName: String,
         callBackUrl: String?,
-        fileHashs: List<Pair<String, FileMetadata>>
+        fileHashes: List<Pair<String, FileMetadata>>
     ): Job {
 
         // Job creation.
@@ -108,13 +87,13 @@ class SignServiceImpl(
                 callBackUrl = callBackUrl,
                 state = JobStateType.INSERTED,
                 stateDate = OffsetDateTime.now(),
-                docsNumber = fileHashs.size,
+                docsNumber = fileHashes.size,
                 channelName = channelName
             )
         ).awaitSingle()
 
         // Merkle tree creation.
-        val merkleTree = TreeBuilder<String>(algorithm.name).elements(fileHashs).build()
+        val merkleTree = TreeBuilder<String>(algorithm.name).elements(fileHashes).build()
         // Compute all merkle tree nodes value.
         SimpleAlgorithmTreeBrowser(algorithm.name).visitTree(merkleTree)
 
@@ -137,71 +116,7 @@ class SignServiceImpl(
             .map { it.toBusiness() as TreeElement.LeafTreeElement }.toList())
     }
 
-    @Transactional
-    override suspend fun getFileProof(requester: Account, fileId: String): Proof? {
-        val leafElement = treeElementRepository.findById(fileId).awaitFirstOrNull() ?: return null
-        val job = jobRepository.findById(leafElement.jobId).awaitSingle()
-        if (!requester.isAdmin && requester.id != job.userId) {
-            throw IllegalAccessException("user ${requester.login} does not have role/permission to get job: ${job.id}")
-        }
-
-        LOGGER.debug("Retrieving proof for document {}.", leafElement.metadata?.fileName)
-
-        val elements = mutableListOf<Pair<String?, TreeElementPosition>>()
-        var nextParent = leafElement.parentId?.let { treeElementRepository.findById(it).awaitFirst() }
-        var element = leafElement
-        while (nextParent != null) {
-            val nextSiblingElement = findSibling(element.id!!, element.parentId)
-            LOGGER.debug("Element ({}) added in siblings list.", nextSiblingElement?.id)
-            elements.add(
-                if (nextSiblingElement == null) {
-                    Pair(
-                        null, if (element.position == TreeElementPosition.RIGHT) {
-                            TreeElementPosition.LEFT
-                        } else {
-                            TreeElementPosition.RIGHT
-                        }
-                    )
-                } else {
-                    Pair(
-                        nextSiblingElement.hash,
-                        nextSiblingElement.position!! // Position is not nullable in a tree element.
-                    )
-                }
-            )
-            element = nextParent
-            nextParent = element.parentId?.let { treeElementRepository.findById(it).awaitFirst() }
-        }
-
-        return Proof(
-            signatureDate = job.injectedDate,
-            filename = leafElement.metadata?.fileName,
-            algorithm = job.algorithm,
-            documentHash = leafElement.hash,
-            rootHash = element.hash,
-            hashes = elements,
-            customFields = leafElement.metadata?.customFields,
-            contractAddress = job.contractAddress,
-            transactionHash = job.transactionHash,
-            blockHash = job.blockHash,
-            signerAddress = job.signerAddress,
-            creatorAddress = job.contractAddress?.let { getContractCreator(it) }
-        )
-    }
-
-    private suspend fun findSibling(id: String, parentId: String?): TreeElementEntity? {
-        if (parentId == null) {
-            return null
-        }
-        val treeElementPredicate = TreeElementCriteria(notId = id, parentId = parentId).toPredicate()
-        return treeElementRepository.findOne(
-            treeElementPredicate
-        ).awaitFirstOrNull()
-
-    }
-
     companion object {
         private val LOGGER = LoggerFactory.getLogger(SignServiceImpl::class.java)
     }
-
 }
