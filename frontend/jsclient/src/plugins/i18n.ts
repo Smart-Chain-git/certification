5ad8a1035755d4d686d5d81137b5e01cac3ee7e2
Yves Coriton <yves.coriton@sword-group.com>
2020-08-10 15:34:36 +0200

    :rotating_light: #150 Linting

--- frontend/jsclient/src/plugins/i18n.ts
+++ frontend/jsclient/src/plugins/i18n.ts
@@ -3,7 +3,7 @@
 */
 
 import Vue from "vue"
-import VueI18n, { LocaleMessages } from "vue-i18n"
+import VueI18n from "vue-i18n"
 
 import fr from "@/locales/fr.json"
 import en from "@/locales/en.json"
@@ -15,15 +15,15 @@ export function getLocale(): string {
 Vue.use(VueI18n)
 
 export default new VueI18n({
-  locale: getLocale(),
-  fallbackLocale: getLocale(),
-  messages: { fr, en },
+    locale: getLocale(),
+    fallbackLocale: getLocale(),
+    messages: {fr, en},
 })
 
 export function tableFooter(i18n: (key: string) => any): any {
     return {
-        "items-per-page-options" : [5, 10, 15, 50],
-        "items-per-page-text": i18n("message.footer.items-per-page-text") + '\xa0',
+        "items-per-page-options": [5, 10, 15, 50],
+        "items-per-page-text": i18n("message.footer.items-per-page-text") +  "\xa0",
         "page-text": i18n("message.footer.page-text"),
     }
 }
