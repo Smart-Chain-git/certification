111b6dec046b549ee136525f96cc3e2d574e5e9d
Vagrant Default User <vagrant@tezos-node-light>
2020-08-18 16:02:33 +0000

    :feature: contract insertion with mickelson

--- scripts/create_contract.sh
+++ scripts/create_contract.sh
@@ -2,11 +2,10 @@
 
 echo "Contract creation"
 
-cd ../contract
-make updateClient
 
 echo "Inscription"
-make originate > originate.log
+./bin/tezos-client -A 127.0.0.1 -P 18731  config update
+./bin/tezos-client -A 127.0.0.1 -P 18731 originate contract hash_timestamping transferring 0 from alice running hash_timestamping.tz --init '{}' --burn-cap 5 --force > originate.log
 CONTRACT_ID=`grep "New contract" originate.log | grep "originated" | cut -d" " -f 3`
 echo
 echo "CONTRACT_ID=["$CONTRACT_ID"]"
@@ -14,15 +13,21 @@ echo
 
 sed "s/TEZOS_CONTRACT_ADDRESS/$CONTRACT_ID/g" ../compose-config/sandbox/application-daemon.yml > ../compose-config/sandbox/application-daemon.yml.tmp
 mv ../compose-config/sandbox/application-daemon.yml.tmp ../compose-config/sandbox/application-daemon.yml
+sed "s/TEZOS_CONTRACT_ADDRESS/$CONTRACT_ID/g" ../compose-config/sandbox/application-rest.yml > ../compose-config/sandbox/application-rest.yml.tmp
+mv ../compose-config/sandbox/application-rest.yml.tmp ../compose-config/sandbox/application-rest.yml
 
 
 
-echo "------------------------------------------------------------"
+echo "daemon------------------------------------------------------------"
 cat ../compose-config/sandbox/application-daemon.yml
+echo "------------------------------------------------------------"
+echo "rest------------------------------------------------------------"
+cat ../compose-config/sandbox/application-rest.yml
 echo "------------------------------------------------------------"
 
 docker restart tezos-digisign-daemon
+docker restart tezos-digisign-rest
 
-echo "tezos-digisign-daemon restarted with ["$CONTRACT_ID"]"
+echo "tezos-digisign-daemon and tezos-digisign-rest restarted with ["$CONTRACT_ID"]"
 
 exit 0
