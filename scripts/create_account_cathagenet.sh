32615e68fe3b12050cd112c2e40ee15cf78f5adf
Bruno CHEVALIER <bruno.chevalier@sword-group.com>
2020-09-14 09:02:02 +0200

    Update create_account_cathagenet.sh

--- scripts/create_account_cathagenet.sh
+++ scripts/create_account_cathagenet.sh
@@ -5,14 +5,15 @@ echo "Account creation"
 if [ $# -eq 0 ]
 then
 
-        echo "Usage $0 <token> <account> <handle> <fullname> <password>"
+        echo "Usage $0 <url> <token> <account> <handle> <fullname> <password>"
         exit 1
 fi
-ACCOUNT=$2
-HANDLE=$3
-FULLNAME=$4
-TOKEN=$1
-PASSWORD=$5
+ACCOUNT=$3
+HANDLE=$4
+FULLNAME=$5
+TOKEN=$2
+URL=$1
+PASSWORD=$6
 
 echo "Inscription"
 echo "ACCOUNT="$ACCOUNT
@@ -20,6 +21,7 @@ echo "FULLNAME="$FULLNAME
 echo "HANDLE="$HANDLE
 echo "TOKEN="$TOKEN
 echo "PASSWORD="$PASSWORD
+echo "URL="$URL
 
 if [ ! -f $HANDLE ]
 then
@@ -57,12 +59,12 @@ echo "--------------------"
  
 
 echo "Add in database"
-curl -X POST "http://rest.tezos-signature.10.20.2.179.xip.io/api/accounts" -o creation_$ACCOUNT.log -H "accept: application/json" -H "origin: me" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{\"login\":\"$ACCOUNT\",\"email\":\"$ACCOUNT@fake.fr\",\"fullName\":\"$FULLNAME\",\"company\":\"none\",\"country\":\"FRANCE\",\"publicKey\":\"$PUBLIC_KEY\",\"hash\":\"$HASH_KEY\",\"isAdmin\":false}"
+curl -X POST $URL"/api/accounts" -o creation_$ACCOUNT.log -H "accept: application/json" -H "origin: me" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{\"login\":\"$ACCOUNT\",\"email\":\"$ACCOUNT@fake.fr\",\"fullName\":\"$FULLNAME\",\"company\":\"none\",\"country\":\"FRANCE\",\"publicKey\":\"$PUBLIC_KEY\",\"hash\":\"$HASH_KEY\",\"isAdmin\":false}"
 
 IDACCOUNT=`cat creation_$ACCOUNT.log | sed 's/,/\n/g' | grep '"id"' | cut -d":" -f2 | sed 's/"//g'`
 
 
-curl -X PATCH "http://rest.tezos-signature.10.20.2.179.xip.io/api/accounts/$IDACCOUNT" -H "accept: application/json" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{\"login\":\"$ACCOUNT\",\"email\":\"$ACCOUNT@fake.fr\",\"password\":\"$PASSWORD\",\"fullName\":\"$FULLNAME\",\"company\":\"none\",\"country\":\"FRANCE\",\"publicKey\":\"$PUBLIC_KEY\",\"hash\":\"$HASH_KEY\",\"isAdmin\":false,\"disabled\":false}"
+curl -X PATCH $URL"/api/accounts/$IDACCOUNT" -H "accept: application/json" -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d "{\"login\":\"$ACCOUNT\",\"email\":\"$ACCOUNT@fake.fr\",\"password\":\"$PASSWORD\",\"fullName\":\"$FULLNAME\",\"company\":\"none\",\"country\":\"FRANCE\",\"publicKey\":\"$PUBLIC_KEY\",\"hash\":\"$HASH_KEY\",\"isAdmin\":false,\"disabled\":false}"
 
 bin/tezos-client transfer 1 from $ACCOUNT to alice  --burn-cap 0.257
 
